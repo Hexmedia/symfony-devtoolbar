1e5a2a1ba3d34627c5bc25eb740687011d458c53
Krystian Kuczek <krystian@hexmedia.pl>
2015-10-15 21:37:29 +0200

    Fix for different ports.

--- panel/app.js
+++ panel/app.js
@@ -1,5 +1,3 @@
-var url = "http://making-wild-eyes.dev/app_dev.php/_profiler/4194ea";
-
 var content = document.getElementById( 'content' );
 
 content.innerHTML = '<iframe id="iframe" src="/panel/loading.html"></iframe>';
@@ -11,10 +9,6 @@ window.onresize = recalculateIframeHeight;
  */
 function recalculateIframeHeight() {
     var iframe = document.getElementById( 'iframe' );
-
-    var width = window.innerWidth
-        || document.documentElement.clientWidth
-        || document.body.clientWidth;
 
     var height = window.innerHeight
         || document.documentElement.clientHeight
@@ -40,17 +34,17 @@ function checkSfResponse() {
          */
         function ( result, isException ) {
             if (!isException) {
-                var url = result[0];
-                var location = result[1];
+                var url = result[0],
+                    location = result[1];
 
                 if (null !== url) {
-
                     var iframe = document.getElementById( 'iframe' );
 
                     url = url.replace( "_wdt", "_profiler" );
 
                     if (currentUrl !== url) {
-                        iframe.src = location.protocol + "//" + location.hostname + url;
+                        iframe.src = location.protocol + "//" + location.hostname + ":" + location.port + url;
+
                         currentUrl = url;
                     }
                 }
@@ -65,4 +59,3 @@ checkSfResponse();
 chrome.devtools.network.onRequestFinished.addListener( function () {
     setTimeout( checkSfResponse, 100 );
 });
-l
